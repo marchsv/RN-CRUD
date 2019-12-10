fe39e6222071d14d5ff63b0085d8f5cbea1d3d85
rogermch <mrchsvnt@gmail.com>
2019-12-10 08:23:26 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react';
+import {Platform, Text, View, StyleSheet} from 'react-native';
+import Navigator from './src/Navigator';
+
+export default class App extends Component {
+  render() {
+    // eslint-disable-next-line prettier/prettier
+    return (
+      <Navigator/>
+    );
+  }
+}
